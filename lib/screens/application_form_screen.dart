08e0d87eeda907ca62357ca7e69d54d7398570d2
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-22 12:36:08 +0800

    Auto-save: lib/screens/application_form_screen.dart

--- lib/screens/application_form_screen.dart
+++ lib/screens/application_form_screen.dart
@@ -6,6 +6,7 @@ import '../data/sample_programmes.dart';
 import '../models/application.dart';
 import '../models/programme.dart';
 import '../utils/validators.dart';
+
 class ApplicationFormScreen extends StatefulWidget {
   const ApplicationFormScreen({super.key, required this.programme});
 
@@ -97,44 +98,44 @@ class _ApplicationFormScreenState extends State<ApplicationFormScreen> {
               // 👈 4.6 — TAMBAH Emel, Telefon, Ringkasan SELEPAS BARIS INI-------
               const SizedBox(height: 14),
 
-TextFormField(
-  controller: _emailCtrl,
-  keyboardType: TextInputType.emailAddress,
-  decoration: const InputDecoration(
-    labelText: 'Emel',
-    hintText: 'nama@contoh.com',
-  ),
-  validator: validateEmail,
-),
+              TextFormField(
+                controller: _emailCtrl,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Emel',
+                  hintText: 'nama@contoh.com',
+                ),
+                validator: validateEmail,
+              ),
 
-const SizedBox(height: 14),
+              const SizedBox(height: 14),
 
-TextFormField(
-  controller: _phoneCtrl,
-  keyboardType: TextInputType.phone,
-  decoration: const InputDecoration(
-    labelText: 'No. Telefon',
-    hintText: '0123456789',
-  ),
-  validator: validatePhoneNumber,
-),
+              TextFormField(
+                controller: _phoneCtrl,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  labelText: 'No. Telefon',
+                  hintText: '0123456789',
+                ),
+                validator: validatePhoneNumber,
+              ),
 
-const SizedBox(height: 14),
+              const SizedBox(height: 14),
 
-TextFormField(
-  controller: _academicCtrl,
-  decoration: const InputDecoration(
-    labelText: 'Ringkasan Keputusan',
-    hintText: 'Cth: SPM 2025 — 9A',
-  ),
-  validator: (value) {
-    if (value == null || value.trim().isEmpty) {
-      return 'Ringkasan keputusan diperlukan';
-    }
+              TextFormField(
+                controller: _academicCtrl,
+                decoration: const InputDecoration(
+                  labelText: 'Ringkasan Keputusan',
+                  hintText: 'Cth: SPM 2025 — 9A',
+                ),
+                validator: (value) {
+                  if (value == null || value.trim().isEmpty) {
+                    return 'Ringkasan keputusan diperlukan';
+                  }
 
-    return null;
-  },
-),
+                  return null;
+                },
+              ),
             ],
           ),
         ),
