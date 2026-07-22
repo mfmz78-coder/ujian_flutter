5f6e4f6887a204714c7586c2f1034f11a91cf770
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-22 11:30:00 +0800

    Auto-save: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'models/programme.dart';
-import 'data/sample_programmes.dart';
-import 'screens/programme_grid_screen.dart';
 import 'screens/home_screen.dart';
-import 'screens/layout_playground_screen.dart';
+import 'screens/programme_detail_screen.dart';
 import 'theme.dart';
 
 class SavedProgrammeCounter extends StatefulWidget {
