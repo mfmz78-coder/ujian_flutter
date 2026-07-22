4bf0a656012f97c52e0763cfe1b1c484ff32c565
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-22 12:33:50 +0800

    Auto-save: lib/screens/application_form_screen.dart

--- lib/screens/application_form_screen.dart
+++ lib/screens/application_form_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../models/application.dart';
 import '../models/programme.dart';
-
+import '../utils/validators.dart';
 class ApplicationFormScreen extends StatefulWidget {
   const ApplicationFormScreen({super.key, required this.programme});
 
