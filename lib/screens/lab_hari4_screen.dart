312d060ad178ede44234412cfd7b63b2c800a48a
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 14:49:12 +0800

    Auto-save: lib/screens/lab_hari4_screen.dart

--- lib/screens/lab_hari4_screen.dart
+++ lib/screens/lab_hari4_screen.dart
@@ -30,13 +30,10 @@ class _LabHari4ScreenState extends State<LabHari4Screen> {
     try {
       final data = await _service.fetchProgrammes();
 
-      _programmes = data;
-      _state = LoadState.loaded;
-      // ignore: avoid_print
-      print(
-        '_state = $_state, '
-        '${_programmes.length} rekod',
-      );
+      setState(() {
+  _programmes = data;
+  _state = LoadState.loaded;
+});
     } catch (_) {
       setState(() {
         _state = LoadState.error;
