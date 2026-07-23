87bc5a58fc1b6b8042d0891aebea6e7e3ff73d92
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 12:33:05 +0800

    Auto-save: lib/screens/lab_hari4_screen.dart

--- lib/screens/lab_hari4_screen.dart
+++ lib/screens/lab_hari4_screen.dart
@@ -41,6 +41,12 @@ class _LabHari4ScreenState extends State<LabHari4Screen> {
   }
 }
 
+@override
+void initState() {
+  super.initState();
+  _load();
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
