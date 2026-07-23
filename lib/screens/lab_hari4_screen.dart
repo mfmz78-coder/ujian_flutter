037f793f383691e2c7635c95156cfe5b60999789
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 12:34:05 +0800

    Auto-save: lib/screens/lab_hari4_screen.dart

--- lib/screens/lab_hari4_screen.dart
+++ lib/screens/lab_hari4_screen.dart
@@ -54,4 +54,27 @@ class _LabHari4ScreenState extends State<LabHari4Screen> {
       body: _buildBody(),
     );
   }
+
+
+  Widget _buildBody() {
+  switch (_state) {
+    case LoadState.idle:
+    case LoadState.loading:
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+
+    case LoadState.error:
+      return const Center(
+        child: Text('Ralat'),
+      );
+
+    case LoadState.loaded:
+      return Center(
+        child: Text(
+          '${_programmes.length} program dimuat',
+        ),
+      );
+  }
+}
 }
