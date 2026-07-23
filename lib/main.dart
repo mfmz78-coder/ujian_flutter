7086512631ab73ec2197343c837076b8255a6bfc
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 11:34:37 +0800

    Auto-save: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,23 @@ class _SavedProgrammeCounterState extends State<SavedProgrammeCounter> {
 }
 
 void main() {
-  runApp(const EttMobileApp());
+  final contoh = Programme.fromJson({
+    'id': 'ETT-999',
+    'universityName': 'Universiti Ujian',
+    'country': 'Egypt',
+    'city': 'Kaherah (Cairo)',
+    'fieldOfStudy': 'Ujian',
+    'studyLevel': 'bachelor',
+    'category': 'spm',
+    'estimatedAnnualCostMyr': 12000,
+    'intakeMonth': 'September',
+    'recognitionNote': 'Ujian sahaja.',
+    'quotaSeats': 10,
+  });
+
+  print(contoh.universityName);
+  print(contoh.estimatedAnnualCostMyr);
+  print(contoh.studyLevel.label);
 }
 
 class ProgrammeSummaryRow extends StatelessWidget {
