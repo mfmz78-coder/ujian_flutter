1ca43095a0fede8ad0c892e8ae41542c3a51d6c9
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 16:03:01 +0800

    Auto-save: lib/services/programme_service.dart

--- lib/services/programme_service.dart
+++ lib/services/programme_service.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../data/sample_programmes.dart';
 import '../models/programme.dart';
+import '../models/application.dart';
 
 class ProgrammeService {
   ProgrammeService({http.Client? client}) : _client = client ?? http.Client();
