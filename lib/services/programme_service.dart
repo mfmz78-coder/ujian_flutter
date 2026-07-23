ff0d26e9d02878f68c8f2ca5aefdb0c154bdecb9
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 11:59:05 +0800

    Auto-save: lib/services/programme_service.dart

--- lib/services/programme_service.dart
+++ lib/services/programme_service.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import '../data/sample_programmes.dart';
 import '../models/programme.dart';
 
 class ProgrammeService {
@@ -32,9 +32,9 @@ class ProgrammeService {
           .toList();
     }
 
-    return [];
+    return _fallback();
   } catch (_) {
-    return [];
+    return _fallback();
   }
 }
 
