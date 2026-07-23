98c8cc76545c27f6b2d9200b5e5c420038cad16d
mfmz78-coder <282175197+mfmz78-coder@users.noreply.github.com>
2026-07-23 12:30:06 +0800

    Auto-save: lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -90,7 +90,7 @@ class EttMobileApp extends StatelessWidget {
       title: 'eTT Mobile',
       debugShowCheckedModeBanner: false,
       theme: KptTheme.light, // tema navy + emas
-      home: const HomeScreen(), // skrin kekal aplikasi
+      home: const LabHari4Screen(), // skrin kekal aplikasi
       onGenerateRoute: (settings) {
         if (settings.name == '/detail') {
           final programme = settings.arguments as Programme;
